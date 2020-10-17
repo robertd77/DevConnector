2bb3c6e7250541e0fe6ae173936c5af511d53b1e
Rob Dominato <robert.dominato@gmail.com>
2020-10-17 18:19:02 -0400

    ready to deploy

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,16 @@ export const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
     const authLinks = (
         <ul>
              <li>
+                 <Link to="/profiles">
+                   Developers
+                 </Link>
+            </li>
+            <li>
+                 <Link to="/posts">
+                   Posts
+                 </Link>
+            </li>
+             <li>
                  <Link to="/dashboard">
                     <i className="fa fa-user"></i>
                      <span className="hide-sm">Dashboard</span>
@@ -24,7 +34,7 @@ export const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
 
     const guestLinks = (
         <ul>
-            <li><Link to="#!">Developers</Link></li>
+            <li><Link to="/developers">Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
         </ul>
